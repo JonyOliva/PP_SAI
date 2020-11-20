bdcf0bd68514e4efcd498ce55e32c3079c9c7d26
Mateo De Benedictis <mateodebenedictismazza@gmail.com>
2020-11-19 22:13:48 -0300

    Prueba para verificar la conexion MVC

--- app/Http/Controllers/InscriptionController.php
+++ app/Http/Controllers/InscriptionController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Instancia;     
 class InscriptionController extends Controller
 {
     public function Index(){
+        //Es para probar la conexion entre Eloquent y las vistas
+        //$test = Instancia::find("1");
+        //return view('inscription')->with('instancia', $test);
         return view('inscription');
     }
 }
