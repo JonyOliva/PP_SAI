526b31ed878448a49b9343a7127772b7b44d002a
oxizakre <70240861+oxizakre@users.noreply.github.com>
2020-12-10 13:25:52 -0300

    Update CarreraController.php

--- app/Http/Controllers/CarreraController.php
+++ app/Http/Controllers/CarreraController.php
@@ -12,6 +12,7 @@ class CarreraController extends Controller
     public function buscarCarreras(Request $request){
 
         //Traer del Negocio la lista de carreras
+        //prueba de push
         $carreras = carrera::where("carr_idinscripcion", $request->idInscripcion)->get();
         return response()->json($carreras);
     }
